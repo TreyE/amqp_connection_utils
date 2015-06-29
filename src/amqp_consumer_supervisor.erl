7129a787b7c7d72cf4921bba5de5f1bd142b7289
Trey Evans <lewis.r.evans@gmail.com>
2015-06-29 11:15:57 -0400

    Added consumer supervisor.

---
+++ src/amqp_consumer_supervisor.erl
@@ -0,0 +1,22 @@
+-module(amqp_consumer_supervisor).
+
+-export([init/1, generate_child_specs/5, start_link/5]).
+
+start_link(ConnectionNameSpec, Subscription, CallbackMod, ModArgs, Count) -> supervisor:start_link(?MODULE, {ConnectionNameSpec, Subscription, CallbackMod, ModArgs, Count}).
+
+init({ConnectionNameSpec, Subscription, CallbackMod, ModArgs, Count}) -> {
+ok,
+{sup_flags(), generate_child_specs(ConnectionNameSpec, Subscription,CallbackMod,ModArgs,Count)}
+}.
+
+generate_child_specs(ConnectionNameSpec, Subscription, CallbackMod, Args, Count) -> lists:map(fun(_I) -> {
+		undefined,
+		{amqp_monitored_consumer, start_link, [ConnectionNameSpec,Subscription,CallbackMod, Args]}
+	} end,
+        lists:seq(1,Count)).
+
+sup_flags() -> {
+  one_for_one,
+  1,
+  5
+}.
